d13c978db196784a2bb4b8b4a8dc079369c8fc27
Stanislav Yakhnenko <s.yakhnenko@qcentris.com>
2022-12-16 08:13:16 +0200

    added email sender

---
+++ Ruby-LPY-lab2/lib/email/email_credentials.rb
@@ -0,0 +1,7 @@
+require_relative '../general/user'
+
+class EmailCredentials
+    def self.get_email_credentials
+        User.new("apikey", "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")
+    end
+end
---
+++ Ruby-LPY-lab2/lib/email/email_sender.rb
@@ -0,0 +1,30 @@
+require_relative '../general/user.rb'
+require 'pony'
+
+module Email
+    class EmailSender
+        def initialize
+            @server = "smtp.sendgrid.net"
+            @port = '587'
+            @from = "noreply@rubylab507.site"
+            @credentials = EmailCredentials.get_email_credentials
+        end
+
+        def send(to, subject, message)
+            Pony.mail({
+                :to => to,
+                :from => @from,
+                :subject => subject,
+                :body => message,
+                :via => :smtp,
+                :via_options => {
+                  :address        => @server,
+                  :port           => @port,
+                  :user_name      => @credentials.username,
+                  :password       => @credentials.password,
+                  :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+                }
+              })
+        end
+    end
+end
