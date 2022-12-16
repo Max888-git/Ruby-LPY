d62fbc137b82756bc1799135a6a1050d8bf3c2a3
Stanislav Yakhnenko <s.yakhnenko@qcentris.com>
2022-12-16 08:15:30 +0200

    fixed to_csv and to_json in lab 1. removed demo file

--- Ruby-LPY-lab1/cart.rb
+++ Ruby-LPY-lab1/cart.rb
@@ -1,6 +1,6 @@
 require_relative './lib/item_container'
+require_relative './item'
 require 'csv'
-require 'nokogiri'
 require 'json'
 
 class Cart
@@ -8,25 +8,18 @@ class Cart
         @items = items
     end
 
-    def write_file(filename, content)
-        File.open(filename, 'w') { |file| file.write(content) }
-    end
-
-    def get_all_files_recursively(root_directory, permitted_extensions)
-        files = Dir.glob("#{root_directory}/**/*").select { |e| File.file? e }
-
-        files.select { |f| permitted_extensions.any? { |ext| ext == File.extname(f)} }
-    end
-
     def save_to_csv()
-        CSV.open("file.csv", "wb") do |csv|
-            page.css('.item').each do |items|
-              name = item.at_css('a').text
-              link = item.at_css('a')[:href]
-              csv << [name, link]
+        csv_string = CSV.generate do |csv|
+            @items.each do |item|
+                csv << [item.book_name, item.author_name, item.price_in_uah, item.publisher, item.image_url]
+            end
+        end
+
+        csv_string
     end
 
     def save_to_json()
+        self.to_json
     end
 
     def save_to_file(filetype)
